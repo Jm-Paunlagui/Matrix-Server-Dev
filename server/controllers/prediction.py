2c8583c6c66d5856f3e8c42c87375133a27ddbf8
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-21 14:51:16 +0800

    Goods, Need input Validation

--- server/controllers/prediction.py
+++ server/controllers/prediction.py
@@ -11,8 +11,7 @@ import tensorflow as tf
 
 from keras.models import load_model
 
-connect_to_matrix = mysql.connector.connect(
-    host="localhost", user="root", password="", database="production_saer")
+connect_to_matrix = mysql.connector.connect(host="localhost", user="root", password="", database="production_saer")
 matrix_cursor = connect_to_matrix.cursor(buffered=True)
 
 # @desc: Get all the tables and columns from a database
@@ -69,10 +68,31 @@ model = load_model("config/model.h5")
 tokenizer = pickle.load(open("config/tokenizer.pickle", "rb"))
 
 
-# @desc: Predict the sentiment of a text and return the result to the database with a new column containing the
-# sentiment, professor name, sentence, and date
+# @desc: Clean the text by removing white
+# spaces, tabs, and new lines
+def clean_text(text):
+    text = text.lower()
+    text = text.strip()
+    text = text.replace("\n", "")
+    text = text.replace("\t", "")
+    text = text.replace("\r", "")
+    return text
+
+
+# @desc: This will retain the apostrophe to the raw data
+def retain_apostrophe(text):
+    text = text.replace("'", "\\'")
+    return text
+
+
+# @desc: Predict the sentiment of a text and return the result to the database
+# with a new column containing the sentiment, professor name, sentence, and
+# date
 @app.route('/analyze_sentiment_from_db', methods=['POST'])
 def analyze_sentiment_from_db():
+    """Analyzes the sentiment of a text and stores the result in the database
+    in the matrix database.
+    """
     # Connect to the database
     if request.is_json:
         host = request.json['host']  # Required
@@ -86,22 +106,20 @@ def analyze_sentiment_from_db():
         course_code = request.json['course_code']  # Required
         input_data_id = request.json['input_data_id']  # Required
 
-        # Required
-        school_year_and_semester = request.json['school_year_and_semester']
+        school_year_and_semester = request.json['school_year_and_semester']  # Required
 
         # Type confirm to confirm the prediction and save it to the database
         type_confirm = request.json['type_confirm']
 
         if type_confirm == input_source:
             try:
-                conn = mysql.connector.connect(
-                    host=host, user=user, password=password, database=database)
+                conn = mysql.connector.connect(host=host, user=user, password=password, database=database)
                 cursor = conn.cursor()
-                cursor.execute("SELECT %s FROM %s" % (input_source, table))
+                cursor.execute("SELECT %s FROM %s where %s IS NOT NULL " % (input_source, table, input_source))
                 data = cursor.fetchall()
 
-                cursor.execute("SELECT %s, %s, %s, %s FROM %s" %
-                               (evaluatee, evaluatee_dept, course_code, input_data_id, table))
+                cursor.execute("SELECT %s, %s, %s, %s FROM %s WHERE %s IS NOT NULL" %
+                               (evaluatee, evaluatee_dept, course_code, input_data_id, table, input_source))
                 info = cursor.fetchall()
 
                 infor_evaluatee = [x[0] for x in info]
@@ -109,15 +127,17 @@ def analyze_sentiment_from_db():
                 infor_course_code = [x[2] for x in info]
                 infor_input_data_id = [x[3] for x in info]
                 data = [x[0] for x in data]
-                raw = [x[0] for x in data]
 
-                # @desc: if the data is NoneType, then it will be replaced with
-                # an empty string to avoid errors when lower casing the data
-                for i in enumerate(data):
-                    if i[1] is None:
-                        data[i[0]] = ""
-                    else:
-                        data[i[0]] = i[1].lower()
+                # @desc: Clean the text
+                data = [clean_text(text) for text in data]
+
+                # @desc: This will be the sentence that will store
+                # to the database instead of tokenize sentence
+                raw = data
+                raw = [retain_apostrophe(text) for text in raw]
+
+                # @desc: Convert the text to lowercase
+                data = [x.lower() for x in data]
 
                 # @desc: Tokenize the data
                 data = tokenizer.texts_to_sequences(data)
@@ -139,17 +159,15 @@ def analyze_sentiment_from_db():
                 # sentiment, analyzed) and the sentiment will be saved
                 # to the database
                 for i in enumerate(predictions):
-                    matrix_cursor.executemany(
+                    matrix_cursor.execute(
                         "INSERT INTO 21_predicted_data "
                         "(`input_source`, `evaluatee`, `evaluatee_dept`, `course_code`, `input_data_id`, "
                         "`input_data`, `input_sentiment`, `is_predicted`, `date_analyzed`, "
                         "`school_year_and_semester`) "
                         "VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(
-                            input_source, infor_evaluatee[i[0]
-                                                          ], infor_evaluatee_dept[i[0]], infor_course_code[i[0]],
-                            infor_input_data_id[i[0]], raw[i[0]
-                                                           ], predictions[i[0]], 1, analyzed,
-                            school_year_and_semester), multi=True)
+                            input_source, infor_evaluatee[i[0]], infor_evaluatee_dept[i[0]], infor_course_code[i[0]],
+                            infor_input_data_id[i[0]], raw[i[0]], predictions[i[0]], 1, analyzed,
+                            school_year_and_semester))
 
                 connect_to_matrix.commit()
                 return jsonify(
